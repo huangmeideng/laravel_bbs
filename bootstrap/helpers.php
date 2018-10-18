3e7782d3f93c6d0baeffd773218eaf0567971e57
huangmeideng <huangmeideng@outlook.com>
2018-10-18 09:16:26 +0000

    用户创建话题

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -9,3 +9,16 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+/**
+ * @author: Kinano
+ * @param $value
+ * @param int $length
+ * @return string
+ * 自动截取字符串
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
