8888db2a516549b20fa9e95c1e753921531a3776
huangmeideng <huangmeideng@outlook.com>
2018-10-18 09:30:20 +0000

    富文本编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>@yield('title','LaravelBBS')-一个基于LARAVEL的BBS</title>
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
@@ -27,5 +28,6 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
