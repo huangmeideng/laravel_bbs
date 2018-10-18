5ea64bc0037c5f1d9294667494d9d49693bdc857
huangmeideng <huangmeideng@outlook.com>
2018-10-18 08:34:44 +0000

    分类列表

--- routes/web.php
+++ routes/web.php
@@ -37,5 +37,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //用户路由
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
-
+//话题路由
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//分类路由
+Route::resource('categories','CategoriesController',['only'=>['show']]);
