e5846c522db87d70f52fe71ddb542e8074b4e2bb
huangmeideng <huangmeideng@outlook.com>
2018-10-19 06:31:16 +0000

    用户回复

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,5 @@ Route::resource('categories','CategoriesController',['only'=>['show']]);
 
 //图片上传
 Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//回复路由
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
